37bae6ec45dce580d3bf51bc8a3bbe02fbde87f0
Katie Sexton <ksexton@hugeinc.com>
2013-10-01 18:58:36 -0400

    Initial commit of old materials

---
+++ slideshow/slides_other/environment.php
@@ -0,0 +1,10 @@
+<div class="slide black invert">
+	<h1 class="white">Environment</h1>
+</div>
+
+<div class="slide">
+	<h4>Github</h4>
+	<ul>
+		<li>https://github.com/hugeinc/sxsw_2013.git</li>
+	</ul>
+</div>	
