770c73584a19d7b96785cdfb1c031adb91b8fc3e
Aaron Chu <arona.chu@gmail.com>
2013-10-03 17:20:10 -0400

    reworked responsive-ness of the page and created the smallest view. Also re-organized PSD layers and added responsive design.

---
+++ website/js/global.js
@@ -0,0 +1,25 @@
+(function(){
+
+	function setUpMobileMenu(){
+
+		var mobiMenuBtn = document.getElementById('mobile-hamburger');
+
+		mobiMenuBtn.onclick = function( e ){
+			e.preventDefault();
+			var allClasses = this.parentNode.className, //caching all classes attached to the element.
+				testClass;
+			if( testClass = allClasses.match(/\sexpanded\s?/i) ){ //test to see if the nav has already been expanded.
+
+				//if the nav is already expanded, we take out the class .expanded.
+				this.parentNode.className = allClasses.substring( 0, allClasses.indexOf(testClass[0]) );
+				return;
+
+			}
+			//if the nav is not expanded, we add calss .expanded.
+			this.parentNode.className += ' expanded';
+		} 
+	}
+
+	setUpMobileMenu();
+
+})();
