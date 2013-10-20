6685b084a3756c60201041e2b47a85ab2851e204
ksex <katie@phobo.us>
2013-10-20 16:08:57 -0400

    Change the js to literal format

--- website/js/global.js
+++ website/js/global.js
@@ -18,13 +18,53 @@ HOLDENS.Global = {
                 return;
 
             }
-            //if the nav is not expanded, we add calss .expanded.
+            //if the nav is not expanded, we add class .expanded.
             this.parentNode.className += ' expanded';
         } 
     }
     
 };
 
+HOLDENS.Map = {
+    init: function() {
+        HOLDENS.Map.drawMap();    
+    },
+    //default options required by the Google Maps APi
+    drawMap: function() {
+        var mapOptions = {
+            zoom: 15,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            mapMaker: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        //Create new google maps
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    
+        var geocoder = new google.maps.Geocoder();
+        
+        //Make a call to geocoder service to create a pin
+        var latlng = geocoder.geocode({
+            address: '50 E Cesar Chavez St, Austin, TX'
+        }, function (results, status) {
+    
+            var myLatLng = results[0].geometry.location;
+    
+            map.setCenter(myLatLng);
+    
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title:"Hello World!"
+            });
+    
+        });    
+    }
+    
+    
+};
+
 window.onload = function() {
     HOLDENS.Global.init();
+    HOLDENS.Map.init();
 }
