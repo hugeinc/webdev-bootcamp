c5cf72b24a3ce5c7c9b80a32e92883af01706507
ksex <katie@phobo.us>
2013-10-20 15:56:27 -0400

    Convert global js to literal

--- website/js/global.js
+++ website/js/global.js
@@ -1,25 +1,30 @@
-(function(){
+var HOLDENS = HOLDENS || {};
+HOLDENS.Global = {
+    init: function() {
+        HOLDENS.Global.setUpMobileMenu();    
+    },
+    setUpMobileMenu: function() {
+        
+        var mobiMenuBtn = document.getElementById('mobile-hamburger');
 
-	function setUpMobileMenu(){
+        mobiMenuBtn.onclick = function( e ){
+            e.preventDefault();
+            var allClasses = this.parentNode.className, //caching all classes attached to the element.
+                testClass;
+            if( testClass = allClasses.match(/\sexpanded\s?/i) ){ //test to see if the nav has already been expanded.
 
-		var mobiMenuBtn = document.getElementById('mobile-hamburger');
+                //if the nav is already expanded, we take out the class .expanded.
+                this.parentNode.className = allClasses.substring( 0, allClasses.indexOf(testClass[0]) );
+                return;
 
-		mobiMenuBtn.onclick = function( e ){
-			e.preventDefault();
-			var allClasses = this.parentNode.className, //caching all classes attached to the element.
-				testClass;
-			if( testClass = allClasses.match(/\sexpanded\s?/i) ){ //test to see if the nav has already been expanded.
+            }
+            //if the nav is not expanded, we add calss .expanded.
+            this.parentNode.className += ' expanded';
+        } 
+    }
+    
+};
 
-				//if the nav is already expanded, we take out the class .expanded.
-				this.parentNode.className = allClasses.substring( 0, allClasses.indexOf(testClass[0]) );
-				return;
-
-			}
-			//if the nav is not expanded, we add calss .expanded.
-			this.parentNode.className += ' expanded';
-		} 
-	}
-
-	setUpMobileMenu();
-
-})();
+window.onload = function() {
+    HOLDENS.Global.init();
+}
